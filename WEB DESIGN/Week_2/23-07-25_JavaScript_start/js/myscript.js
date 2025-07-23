bc3789aec8c828f2d2d97c6652032bdd8dccd660
Roshan Dalvi <roshandalvi16@gmail.com>
2025-07-23 11:59:06 +0530

    password match task

--- WEB DESIGN/Week_2/23-07-25_JavaScript_start/js/myscript.js
+++ WEB DESIGN/Week_2/23-07-25_JavaScript_start/js/myscript.js
@@ -3,3 +3,10 @@ function test() {
     alert("button");
     // document.write("Welcome");
 }
+
+function add() {
+    var n1 = Number(document.form1.num1.value);
+    var n2 = Number(document.form1.num2.value);
+    var res = n1 + n2;
+    document.write(res);
+}
